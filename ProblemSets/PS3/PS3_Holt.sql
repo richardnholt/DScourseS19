a311ab8f48a3004f2a0eb341bfe8261d86bef2ea
Richard Holt <42751936+richardnholt@users.noreply.github.com>
2019-02-05 14:30:49 -0600

    Update PS3_Holt.sql

--- ProblemSets/PS3/PS3_Holt.sql
+++ ProblemSets/PS3/PS3_Holt.sql
@@ -1,6 +1,24 @@
 --Read initial CSV
-.mode csv;
-.import /DScourseS19/ProblemSets/PS3/FL_insurance_sample.csv FLInsur;
+CREATE TABLE FLInsur(
+  "policyID",
+  "statecode",
+  "county,eq_site_limit",
+  "hu_site_limit",
+  "fl_site_limit",
+  "fr_site_limit",
+  "tiv_2011",
+  "tiv_2012",
+  "eq_site_deductible",
+  "hu_site_deductible",
+  "fl_site_deductible",
+  "fr_site_deductible",
+  "point_latitude",
+  "point_longitude",
+  "line,construction",
+  "point_granularity" 
+);
+.mode csv
+.import FL_insurance_sample.csv FLInsur
 --Print out the first ten rows
 SELECT * FROM FLInsur LIMIT 10;
 --List out the counties in the sample
