653ad69a717bc14ff5390efbb13809df3c545932
Richard Holt <richard.n.holt@ou.edu>
2019-03-12 15:30:18 -0500

    PS7 push

--- ProblemSets/PS6/PS6_Holt.R
+++ ProblemSets/PS6/PS6_Holt.R
@@ -1,3 +1,4 @@
+#install.packages('stringi')
 library(twitteR)
 library(tm)
 library(wordcloud)
@@ -10,6 +11,8 @@ library(forcats)
 library(dplyr)
 library(rvest)
 library(stringr)
+library(stringi)
+library(vcd)
 
 webpage <- read_html("https://www.imdb.com/list/ls021105452/")
 results <- webpage %>%
@@ -19,18 +22,41 @@ records <- vector("list", length = length(results))
 for (i in seq_along(results)) {
   rank <- results[i] %>% 
     html_nodes(".text-primary") %>% 
-    html_text(trim = TRUE)
+    html_text(trim = TRUE) %>%
+    str_remove(pattern = "\\.")
   title <- results[i] %>%
     html_nodes(".lister-item-header") %>%
     html_text(trim = TRUE) %>%
-    str_sub(13)
+    str_remove(pattern = "[0-9].\n    \n    ") %>%
+    str_remove(pattern = "\n   ") %>%
+    str_remove(pattern = " \\(2018\\)") %>%
+    str_remove(pattern = " \\(2017\\)")
+  runtime <- results[i] %>%
+    html_nodes(".runtime") %>%
+    html_text(trim = TRUE) %>%
+    str_remove(pattern = " min")
   genre <- results[i] %>%
     html_nodes(".genre") %>%
-    html_text(trim = TRUE)
-  #rating <- results[i] %>%
-   # html_nodes(".ipl-rating-star__rating") %>%
-    #html_text(trim = TRUE)
-  records[[i]] <- data_frame(rank=rank,title=title,genre=genre)
+    html_text(trim = TRUE) %>%
+    str_split(", ")
+  rating <- results[i] %>%
+    html_nodes(".ratings-metascore") %>%
+    html_text(trim = TRUE) %>%
+    str_remove(pattern = "        \n        Metascore")
+  records[[i]] <- data_frame(rank=rank,title=title,runtime=runtime,genre=genre,rating=rating)
 }
 df <- bind_rows(records)
+for (i in 10:83){
+  df$title[[i]] <- str_sub(df$title[[i]],2)
+}
 df
+time <- hist(as.numeric(df$runtime),breaks=seq(50,160,5))
+plot(time,main=paste("Histogram of Runtimes"),xlab="Minutes")
+
+qplot(as.numeric(runtime), as.numeric(rank), data=df, 
+      main="Rating by Runtime",
+      xlab="Runtime", ylab="Rating")
+
+qplot(as.numeric(rating), as.numeric(rank), data=df, 
+      main="Rank by Rating",
+      xlab="Rating", ylab="Rank")
--- ProblemSets/PS7/PS7_Holt.R
+++ ProblemSets/PS7/PS7_Holt.R
@@ -1,5 +1,41 @@
 library(dplyr)
 library(mice)
 library(stargazer)
-read_csv(wages.csv)
+library(tidyr)
+library(HotDeckImputation)
+library(magrittr)
+table <- read.csv('wages.csv') %>%
+  drop_na(tenure) %>%
+  drop_na(hgc)
+stargazer(table)
 
+#Regression using complete cases
+imp.deletion <- lm(logwage ~ hgc + tenure + I(tenure^2) + age + married, data=table, na.action = na.omit)
+print(summary(imp.deletion))
+
+#Mean imputation with missing logwages
+table$mean_logwage <- table$logwage
+table %<>% mutate(mean_logwage = logwage)
+x <- mean(table$logwage,na.rm=TRUE)
+table$mean_logwage[is.na(table$mean_logwage)] <- x
+
+imp.mean <- lm(mean_logwage ~ hgc + college + tenure + I(tenure^2) + age + married, data=table, na.action=na.omit)
+print(summary(imp.mean))
+
+#Imputation from the complete cases regression
+table$predict_logwage <- table$logwage
+test <- lm(logwage ~ hgc + college + tenure + I(tenure^2) + age + married, data=table, na.action = na.omit)
+table$preds <- NA
+table$preds[!is.na(table$hgc) & !is.na(table$tenure)] <- predict(test, table, na.action=na.exclude)
+table$predict_logwage[is.na(table$logwage)] <- table$preds[is.na(table$logwage)]
+
+imp.predict <- lm(predict_logwage ~ hgc + college + tenure + I(tenure^2) + age + married, data=table, na.action=na.omit)
+print(summary(imp.predict))
+
+#Mice package multiple imputation regression
+imp.mice <- mice(table, seed = 123456)
+fit = with(imp.mice, lm(logwage ~ hgc + tenure))
+round(summary(pool(fit)),2)
+
+# Final Stargazer table
+stargazer(imp.deletion, imp.mean, imp.predict)
